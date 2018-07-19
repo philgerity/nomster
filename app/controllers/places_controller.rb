12081b31ef1ceea2627999426b45cd5a408c020e
Phil Gerity <philip.gerity@outlook.com>
2018-07-19 01:52:09 +0000

    validations

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,9 +10,12 @@ class PlacesController < ApplicationController
   end
 
   def create
-    Place.create(place_params)
-    current_user.places.create(place_params)
-    redirect_to root_path
+    @place = current_user.places.create(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -30,7 +33,11 @@ class PlacesController < ApplicationController
     end
 
     @place.update_attributes(place_params)
-    redirect_to root_path
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
