0e3155597865bcc63b863f0f88ab278bb0ff65e9
Phil Gerity <philip.gerity@outlook.com>
2018-08-17 03:04:25 +0000

    ratings dropdown

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,17 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :place
+
+RATINGS = {
+  '1/5': '1_star',
+  '2/5': '2_stars',
+  '3/5': '3_stars',
+  '4/5': '4_stars',
+  '5/5': '5_stars'
+}
+
+  def humanized_rating
+    RATINGS.invert[self.rating]
+  end
+
 end
