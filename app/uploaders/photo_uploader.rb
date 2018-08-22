10589a1aad5aab65a1b92ca918bfe5e9917eea02
Phil Gerity <philip.gerity@outlook.com>
2018-08-22 15:47:04 +0000

    add photo uploader

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,4 +1,4 @@
-class AvatarUploader < CarrierWave::Uploader::Base
+class PhotoUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
